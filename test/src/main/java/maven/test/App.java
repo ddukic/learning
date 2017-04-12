a4ef5b9d62584a326335d53cd0c46b34ecb53084
Dane <ddukic86@gmail.com>
2017-04-12 16:31:59 +0200

    My first edit of java file

--- test/src/main/java/maven/test/App.java
+++ test/src/main/java/maven/test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "This is a simple change of java file just to see how git do things." );
     }
 }
